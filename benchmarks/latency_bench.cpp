3f1b097c6eb73953c8fc10f77a1b5c3decac5154
Siddhesh Badnapurkar <173941233+51ddhesh@users.noreply.github.com>
2026-04-24 00:36:55 +0530

    add BSL to benchmark

--- benchmarks/latency_bench.cpp
+++ benchmarks/latency_bench.cpp
@@ -1,3 +1,8 @@
+// Copyright 2026 51ddhesh
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <benchmark/benchmark.h>
 #include <vector>
 #include <memory>
